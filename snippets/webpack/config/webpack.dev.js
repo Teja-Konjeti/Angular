ac34a02925ab216865347fdb2c1c507774200989
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2016-11-24 22:09:25 +0530

    added webpack in snippets

---
+++ snippets/webpack/config/webpack.dev.js
@@ -0,0 +1,28 @@
+
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+
+const path = require('path');
+const rootDir = path.resolve(__dirname, '..');
+
+module.exports = webpackMerge(commonConfig, {
+
+devtool: 'cheap-module-eval-source-map',
+
+output: {
+    path: path.resolve(rootDir, 'dist'),
+    publicPath: 'http://localhost:8080/',
+    filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
+});
